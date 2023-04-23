de700bfc34f66e980480a56ed55ba19f48bc01d8
hhanzou <erickvictor1404@gmail.com>
2023-04-23 14:52:43 -0300

    individual pages ok; going to do delete service

--- webGames/src/app/app-routing.module.ts
+++ webGames/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './components/pages/home/home.component';
 import { AddGamesComponent } from './components/pages/add-games/add-games.component';
 import { AddCategoriesComponent } from './components/pages/add-categories/add-categories.component';
 import { GamedetailsComponent } from './components/pages/gamedetails/gamedetails.component';
+import { EditgameComponent } from './components/pages/editgame/editgame.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'add-games', component: AddGamesComponent},
   {path: 'add-categories', component: AddCategoriesComponent},
-  {path: 'gamedetails/id', component: GamedetailsComponent}
+  {path: 'gamedetails/:id', component: GamedetailsComponent},
+  {path: 'editgame/:id', component: EditgameComponent}
 ];
 
 @NgModule({
--- webGames/src/app/components/pages/gamedetails/gamedetails.component.ts
+++ webGames/src/app/components/pages/gamedetails/gamedetails.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { GamesService } from 'src/app/services/games.service';
+import { Game } from 'src/app/Game';
 
 @Component({
   selector: 'app-gamedetails',
   templateUrl: './gamedetails.component.html',
   styleUrls: ['./gamedetails.component.css']
 })
-export class GamedetailsComponent {
+export class GamedetailsComponent implements OnInit {
+  game?: Game;
+  
+  constructor(private gameService: GamesService, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    console.log(id)
+
+    this.gameService
+    .GetById(id)
+    .subscribe(result => (this.game = result));    
+  }
 
 }
